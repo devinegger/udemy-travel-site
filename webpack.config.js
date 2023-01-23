fd0f7a8b6b5b7c4384f67e23b573f152b11022f0
devinegger <devin.egger@icloud.com>
2023-01-23 10:36:56 -0500

    added postcss plugins and setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+    require('postcss-extend-rule')
+]
+
+module.exports= {
+    entry: './app/assets/scripts/App.js',
+    mode: 'development',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins} } } ] 
+            }
+        ]
+    }
+}
